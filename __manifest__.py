6be6ae7c59c5f7797aa64efcd04be1dc77b01670
Bhavin1070 <bsp1070@gmail.com>
2020-01-23 21:29:52 +0530

    Dashoard

--- __manifest__.py
+++ __manifest__.py
@@ -4,10 +4,11 @@
 	'description' : 'This Application Provide online printing services and couriar services',
 	'author' : 'Bhavin Patel',
 	'version' : '0.1',
-	'depends' : ['base'],
+	'depends' : ['base', 'web_dashboard'],
 	'data' : [
 		'security/ir.model.access.csv',
 		'data/data.xml',
+		'report/report.xml',
 		'views/main_template.xml',
 	],
 	'demo' : [
--- models/models.py
+++ models/models.py
@@ -43,24 +43,41 @@ class Inquiry(models.Model):
 	_name = 'inquiry.inquiry'
 	_description = 'Inquiry Details'
 
+	name = fields.Char(string="Inquiry Name", required=True)
 	object_id = fields.Many2one(string="Print Object", comodel_name="object.object", required=True)
-	attachment = fields.Binary(string="File", required=True)
+	cust_id = fields.Many2one(string="User Name", comodel_name="user.user", required=True)
+	attachment = fields.Binary(string="File")
 	provider_id = fields.Many2one(string="Provider Name", comodel_name="provider.provider", required=True)
 	location = fields.Char(string="Delivery Location", required=True)
 	remark = fields.Char(string="Remark")
 
 
-class JobWork(models.Model):
-	_name = 'jobwork.jobwork'
-	_description = 'JobWork Details'
+class Order(models.Model):
+	_name = 'order.order'
+	_description = 'Order Details'
+	_rec_name = 'inquiry_id'
 
+	start = fields.Datetime(string="Start Printing")
+	end = fields.Datetime(string="End Printing")
+	price = fields.Float(string="Price")
+	state = fields.Selection([('pending', 'Pending'), ('progress', 'In Progress'), ('complated', 'Complated')], string="Status", default="pending")
 	inquiry_id = fields.Many2one(string="Inquiry Name", comodel_name="inquiry.inquiry", required=True)
-	start = fields.Datetime(string="Start Printing", required=True)
-	end = fields.Datetime(string="End Printing", required=True)
-	price = fields.Float(string="Price", required=True)
-	customer_id = fields.Many2one(string="Customer Name", comodel_name="user.user", required=True)
+	object_id = fields.Many2one(string="Print Object", comodel_name="object.object", related="inquiry_id.object_id", store=True)
+	cust_id = fields.Many2one(string="Customer Name", comodel_name="user.user", related="inquiry_id.cust_id")
 	location = fields.Char(string="Delivery Location", related="inquiry_id.location")
 	attachment = fields.Binary(string="File", related="inquiry_id.attachment")
 	provider_id = fields.Many2one(string="Provider Name", related="inquiry_id.provider_id")
 	remark = fields.Char(string="Remark", related="inquiry_id.remark")
+
+	def action_pending(self):
+		self.write({'state' : 'pending'})
+		return True
+	
+	def action_progress(self):
+		self.write({'state' : 'progress'})
+		return True
+	
+	def action_complated(self):
+		self.write({'state' : 'complated'})
+		return True
 
